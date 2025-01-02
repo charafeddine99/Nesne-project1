ef3a6b4f2229e4b9bf15989b1ecfbbd818ff5775
CanerPekkan54 <canerp5454@gmail.com>
2025-01-02 20:12:51 +0300

    Harita Sınıfı(Map Class)
    Oyunun harita sınıfını oluşturdum

--- Caner.cpp
+++ Caner.cpp
@@ -44,3 +44,44 @@ public:
     int getX() const { return x; } // Oyuncunun x koordinatını döndürür
     int getY() const { return y; } // Oyuncunun y koordinatını döndürür
 };
+
+// Harita sinifi
+class Map {
+private:
+    vector<vector<char>> grid; // Harita için 2D karakter matrisi
+    int size; // Harita boyutu
+    pair<int, int> treasurePosition; // Hazine konumu
+
+public:
+    Map(int size) : size(size) { // Yapıcı fonksiyon, harita boyutunu ayarlar
+        grid = vector<vector<char>>(size, vector<char>(size, '.')); // Harita nokta (‘.’) ile doldurulur
+        placeTreasure(); // Hazineyi yerleştirir
+    }
+
+    void placeTreasure() { // Hazineyi haritada rastgele bir yere yerleştirir
+        treasurePosition.first = rand() % size; // Rastgele x koordinatı
+        treasurePosition.second = rand() % size; // Rastgele y koordinatı
+        grid[treasurePosition.first][treasurePosition.second] = 'T'; // Hazineyi ‘T’ ile işaretler
+    }
+
+    bool isTreasure(int x, int y) { // Oyuncunun koordinatlarında hazine olup olmadığını kontrol eder
+        return x == treasurePosition.first && y == treasurePosition.second;
+    }
+
+    void display(Player& player) { // Haritayı ekrana yazdırır
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == player.getY() && j == player.getX()) {
+                    cout << 'P' << " "; // Oyuncuyu ‘P’ ile gösterir
+                }
+                else if (i == treasurePosition.first && j == treasurePosition.second) {
+                    cout << 'T' << " "; // Hazineyi ‘T’ ile gösterir
+                }
+                else {
+                    cout << grid[i][j] << " "; // Boş alanı nokta ile gösterir
+                }
+            }
+            cout << endl;
+        }
+    }
+};
